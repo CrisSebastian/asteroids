744d510d8197a45e85c72a0780a24c409886f453
CrisSebastian <cristofolisebastian@gmail.com>
2025-02-05 22:26:00 -0300

    added circleshap, player, and now we draw the player in the main()

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     delta = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         delta = clock.tick(60) / 1000
